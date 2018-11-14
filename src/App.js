42d6dd753d14d75d6ad314ea1da5ec49193003bd
Quentin <qburg@cutii.io>
2018-11-14 17:59:03 +0100

    :sparkles: add tab menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import { createBottomTabNavigator } from 'react-navigation';
+import React from 'react';
 import Home from 'Home';
 import CallApela from 'CallApela';
-// import {Icnon}
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default createBottomTabNavigator({
-  Catalogue: Home,
-  Commander: CallApela,
-});
+export default createBottomTabNavigator(
+  {
+    Catalogue: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="shopping-cart" size={28} color={tintColor} />
+        ),
+      },
+    },
+    Appeler: {
+      screen: CallApela,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="phone" size={28} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      labelStyle: { fontSize: 15, color: '#d1603d', fontWeight: 'bold' },
+      style: { backgroundColor: '#f6f5ae' },
+      activeTintColor: '#e91e63',
+      // activeBackgroundColor: '#e91e63',
+    },
+  }
+);
